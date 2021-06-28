60768241cc8ab1ec2679bd5df4c12a89c6c89f96
Znowy <38059278+Znowy@users.noreply.github.com>
2021-06-28 15:46:11 -0700

    Added Nameplate (Includes name and Healthbar)

--- 3Dtestgame/Assets/Scripts/Target.cs
+++ 3Dtestgame/Assets/Scripts/Target.cs
@@ -1,19 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
-    public float health = 100f;
-    public int ammoGiven = 50;
+    public enum Relationship {Friendly, Enemy, Neutral};
+    private Color[] colorState = new Color[] { Color.green, Color.red, Color.blue };
+
+    public string enemyName;
+    public float maxHealth;
+    public float health;
+    public int ammoGiven;
+    public Relationship targetRelationship;
+
+    public RectTransform healthbar;
+    public float healthbarWidth;
+
+    public Text nameText;
 
     public SpawnItem itemSpawner;
+
+    private void Start()
+    {
+        nameText.text = enemyName;
+        nameText.color = colorState[(int)targetRelationship];
+    }
 
     public void ReceiveDamage(float amount)
     {
         health -= amount;
+
         if (health <= 0f)
         {
             Die();
         }
+
+        healthbar.sizeDelta = new Vector2(health / (maxHealth / healthbarWidth), healthbar.sizeDelta.y);
     }
 
     void Die()
