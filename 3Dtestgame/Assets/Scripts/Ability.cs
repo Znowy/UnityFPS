dbc5ee108ded38db3510394d5bad17aba043ce59
Jake Knudsen <jknudsen99@gmail.com>
2023-02-27 18:45:47 -0800

    Started work on creating an ability

---
+++ 3Dtestgame/Assets/Scripts/Ability.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+    public float abilityCooldown = 10f;
+    public float abilityDamage = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
